cbcf19402093265ee8d056180792dac94d9c6a35
Jas Spencer <jas.spencer414@gmail.com>
2020-01-03 23:50:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,7 @@ def save
       breed = row[2]
       self.new(id: id, name: name, breed: breed)
     end
-  
+
     def self.find_by_name(name)
       sql = <<-SQL
         SELECT *
@@ -80,12 +80,12 @@ def save
         WHERE name = ?
         LIMIT 1
       SQL
-  
+
       DB[:conn].execute(sql,name).map do |row|
         self.new_from_db(row)
       end.first
     end
-  
+
     def self.find_by_id(id)
       sql = <<-SQL
         SELECT *
@@ -93,12 +93,12 @@ def save
         WHERE id = ?
         LIMIT 1
       SQL
-  
+
       DB[:conn].execute(sql,id).map do |row|
         self.new_from_db(row)
       end.first
     end
-  
+
     def update
       sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
       DB[:conn].execute(sql, self.name, self.breed, self.id)
